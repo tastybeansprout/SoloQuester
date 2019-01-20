3c14c172b96e86f721a2551c84049e7f60dc17e0
Luke S <tastybeansprout@gmail.com>
2019-01-20 16:14:37 -0500

    Update tabulator-script.js

--- js/tabulator-script.js
+++ js/tabulator-script.js
@@ -318,7 +318,7 @@ var table = new Tabulator("#example-table", {
         {title:"", field:"thumbnail", formatter:"image", headerSort:false, width: 130, formatterParams:{
             width:"100px",   
         }},
-        {title:"Name", field:"name", formatter:"textarea", cssClass:"name-styling", width:200},
+        {title:"Name", field:"name", formatter:"textarea", cssClass:"name-styling"},
         {title:"Rating", field:"average"}, 
         {title:"Reviewers", field:"statistics"}, 
         {title:"Rank", field:"value"},
